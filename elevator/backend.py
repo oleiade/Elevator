625ec28d6f561e0a470170baac764817480b105b
Theo Crevon <tcrevon@gmail.com>
2013-01-29 17:19:34 +0100

    Update #121: fixed workers stop action

--- elevator/backend.py
+++ elevator/backend.py
@@ -9,12 +9,14 @@ import uuid
 import logging
 import threading
 
+from collections import defaultdict
+
 from .db import DatabasesHandler
 from .env import Environment
 from .api import Handler
-from .constants import FAILURE_STATUS, REQUEST_ERROR, WORKER_HALT,\
+from .constants import SUCCESS_STATUS, FAILURE_STATUS, REQUEST_ERROR,\
                        WORKER_STATUS, WORKER_ALIVE, WORKER_START,\
-                       WORKER_LAST_ACTION
+                       WORKER_HALT, WORKER_LAST_ACTION
 from .message import Request, MessageFormatError, ResponseContent, ResponseHeader
 from .utils.snippets import sec_to_ms
 from .utils.patterns import enum
@@ -31,7 +33,8 @@ class Worker(threading.Thread):
     def __init__(self, zmq_context, databases, *args, **kwargs):
         threading.Thread.__init__(self)
         self.instructions = {
-            WORKER_STATUS: self.status_inst,
+            WORKER_STATUS: self._status_inst,
+            WORKER_HALT: self._stop_inst,
         }
         self.uid = uuid.uuid4().hex
         self.env = Environment()
@@ -54,14 +57,21 @@ class Worker(threading.Thread):
         self.remote_control_socket.connect('inproc://remote')
         self.remote_control_socket.send_multipart([self.uid])
 
-    def status_inst(self):
+    def _status_inst(self):
         return str(self.state)
+
+    def _stop_inst(self):
+        return self.stop()
 
     def handle_instruction(self):
         try:
             instruction = self.remote_control_socket.recv_multipart(flags=zmq.NOBLOCK)[0]
             response = self.instructions[instruction]()
-            return self.remote_control_socket.send_multipart([response], flags=zmq.NOBLOCK)
+            self.remote_control_socket.send_multipart([response], flags=zmq.NOBLOCK)
+
+            if instruction == WORKER_HALT and int(response) == SUCCESS_STATUS:
+                raise HaltException()
+            return
         except zmq.ZMQError as e:
             if e.errno == zmq.EAGAIN:
                 return
@@ -72,26 +82,16 @@ class Worker(threading.Thread):
         msg = None
 
         while (self.state == self.STATES.RUNNING):
-            self.handle_instruction()
+            try:
+                self.handle_instruction()
+            except HaltException:
+                break
 
             try:
                 sender_id, msg = self.backend_socket.recv_multipart(copy=False, flags=zmq.NOBLOCK)
-                # If worker pool sends a WORKER_HALT, then close
-                # and return to stop execution
-                if sender_id.bytes == WORKER_HALT:  # copy=False -> zmq.Frame
-                    raise HaltException("Gracefully stopping worker %r" % self.ident)
             except zmq.ZMQError as e:
                 if e.errno == zmq.EAGAIN:
                     continue
-                else:
-                    self.state = self.STATES.STOPPED
-                    errors_logger.warning('Worker %r encountered and error,'
-                                          ' and was forced to stop' % self.ident)
-                    return
-            except HaltException as e:
-                activity_logger.info(e)
-                return self.close()
-
             self.running = True
 
             try:
@@ -112,11 +112,14 @@ class Worker(threading.Thread):
             self.backend_socket.send_multipart([sender_id, header, response], flags=zmq.NOBLOCK, copy=False)
             self.running = False
 
-    def close(self):
+    def stop(self):
         self.state = self.STATES.STOPPED
 
         if not self.backend_socket.closed:
             self.backend_socket.close()
+
+        activity_logger.info("Gracefully stopping worker %s" % self.uid)
+        return str(SUCCESS_STATUS)
 
 
 class Supervisor(object):
@@ -127,7 +130,7 @@ class Supervisor(object):
     """
     def __init__(self, zmq_context, databases_store, timeout=3):
         self.databases_store = databases_store
-        self.workers = {}
+        self.workers = defaultdict(dict)
         self.timeout = sec_to_ms(timeout)
 
         self.zmq_context = zmq_context
@@ -139,11 +142,9 @@ class Supervisor(object):
         self.poller.register(self.socket, zmq.POLLIN)
 
     def __del__(self):
-        while any(worker.isAlive() for worker in self.pool):
-            self.socket.send_multipart([WORKER_HALT, ""])
-
-        for worker in self.pool:
-            worker.join()
+        for worker_id, worker in self.workers.iteritems():
+            self.stop(worker_id)
+            worker['thread'].join()
 
     def command(self, instruction, workers_ids=None):
         """Command an action to workers.
@@ -158,7 +159,7 @@ class Supervisor(object):
 
         for worker_id in workers_ids:
             if worker_id in self.workers:
-                self.socket.send_multipart([self.workers[worker_id], instruction], flags=zmq.NOBLOCK)
+                self.socket.send_multipart([self.workers[worker_id]['socket'], instruction], flags=zmq.NOBLOCK)
 
                 retried = 0
                 while retried <= max_retries:
@@ -180,6 +181,12 @@ class Supervisor(object):
     def statuses(self):
         return self.command(WORKER_STATUS)
 
+    def stop(self, worker_id):
+        return self.command(WORKER_HALT, [worker_id])
+
+    def stop_all(self):
+        return self.command(WORKER_HALT)
+
     def init_workers(self, count):
         pos = 0
 
@@ -188,7 +195,8 @@ class Supervisor(object):
             worker = Worker(self.zmq_context, self.databases_store)
             worker.start()
             socket_id, worker_id = self.socket.recv_multipart()
-            self.workers[worker_id] = socket_id
+            self.workers[worker_id]['socket'] = socket_id
+            self.workers[worker_id]['thread'] = worker
             pos += 1
 
 
