5bf0f4e6002ae01ef44b713661f231b30398205b
Theo Crevon <tcrevon@gmail.com>
2013-03-18 11:58:14 +0100

    Fix: pyzmq version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'lz4',
         'python-daemon==1.5.5',
         'msgpack-python',
-        'pyzmq',
+        'pyzmq==13.0.0',
         'unittest2',
         'ujson',
         'procname',
